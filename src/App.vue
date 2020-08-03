b53f98284834c2a99874b27dd7de3dbaeb6cdf42
twoheart <twoheart@vip.qq.com>
2020-08-03 15:21:59 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Haha />
+</template>
+
+<script>
+import Haha from './components/Haha.vue';
+export default {
+  name: 'App',
+  components: {
+    Haha
+  }
+}
+</script>
---
+++ src/components/Haha.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>this is a new hello</div>
+</template>
+<script>
+  export default {
+    name: 'Haha'
+  }
+</script>
