e8a333354484ac65468068b0d8378d00bbd7bfc0
twoheart <twoheart@vip.qq.com>
2020-08-04 15:57:34 +0800

    Home 页面样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <TopNav />
+    <TopNav/>
     <div class="banner">
       <h1>FAN UI</h1>
       <h2>一个简洁的 UI 框架</h2>
-      <p class="sections">
-        <a>GitHub</a>
-        <a>开始</a>
+      <p class="actions">
+        <a href="https://github.com/twoheartliu">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -14,6 +14,7 @@
 
 <script lang="ts">
 import TopNav from '../components/TopNav.vue';
+
 export default {
   name: 'Home',
   components: {
@@ -23,5 +24,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+  flex-direction: column;
+  background: lightgreen;
+  .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h / 2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
