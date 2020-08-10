f7d3b8fe33bc9c1a0e38250c80d5e74bc9d045ec
twoheart <twoheart@vip.qq.com>
2020-08-10 17:36:28 +0800

    添加了切换动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -39,6 +39,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 
 button.checked {
@@ -47,5 +48,9 @@ button.checked {
 
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
 </style>
