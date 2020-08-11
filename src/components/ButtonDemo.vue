4c16c3f706e98a3eb71b353cc5f82bb6b210e891
twoheart <twoheart@vip.qq.com>
2020-08-11 18:40:39 +0800

    Button 组件区分事件监听区域

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,15 +2,22 @@
   <div>
     <h1>Button组件</h1>
     <h2>示例1</h2>
-    <Button>你好</Button>
+    <Button @click="handleClick" @mouseover="handleClick" @focus="handleClick" size="small">你好</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue';
+
 export default {
   name: 'ButtonDemo',
-  components: {Button}
+  components: {Button},
+  setup() {
+    const handleClick = () => {
+      console.log('buttonClick');
+    };
+    return {handleClick};
+  }
 };
 </script>
 
