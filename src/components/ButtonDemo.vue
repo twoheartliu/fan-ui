6fd55071cc8771008ea651761d6b50f43e567f6e
twoheart <twoheart@vip.qq.com>
2020-08-12 10:36:25 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,12 +2,10 @@
   <div>
     <h1>Button组件</h1>
     <h2>示例1</h2>
-    <Button
-        @click="handleClick"
-        @mouseover="handleClick"
-        @focus="handleClick"
-    >你好
-    </Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>
+  <button class="fan-button" :class="`theme-${theme}`">
     <slot/>
   </button>
 </template>
@@ -7,6 +7,12 @@
 <script lang="ts">
 export default {
   name: 'Button',
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
 };
 </script>
 
