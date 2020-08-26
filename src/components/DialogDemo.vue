823e53232e0f102caa146ab57eedf82cc413e773
twoheart <twoheart@vip.qq.com>
2020-08-26 16:23:53 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,20 @@
 <template>
-   <div>dialog</div>
+  <section>
+    <h2>代码演示</h2>
+    <section>
+      <h2>示例1</h2>
+      <Dialog />
+    </section>
+    <section>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue';
 export default {
-  name: "DialogDemo"
+  name: "DialogDemo",
+  components: {
+    Dialog
+  }
 }
 </script>
 
